39f3604b7d369203266670445f8f9e23ab048666
JJMEJIAAVILA <jhon_jmejia@soy.sena.edu.co>
2025-11-12 15:28:09 -0500

    primer commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Efecto de aparición al hacer scroll
+const fadeEls = document.querySelectorAll('.fade-in');
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+
+fadeEls.forEach(el => observer.observe(el));
